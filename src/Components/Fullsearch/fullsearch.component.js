ab1294ea55b44e3c65686be8861e8dff5a67bdc1
Günsel Akkemik <35037442+gunsela92@users.noreply.github.com>
2021-02-09 11:22:33 +0300

    Added sorting tabledata

--- src/Components/Fullsearch/fullsearch.component.js
+++ src/Components/Fullsearch/fullsearch.component.js
@@ -1,10 +1,13 @@
 import React, { useState } from "react";
 import Service from "../Service/service";
 import logo from "../../assets/img/logo.png";
+import sorter from "../../assets/img/sorter.png";
 
 const Fullsearch = () => {
   const [tableData, settableData] = useState([]);
   const [loading, setloading] = useState(false);
+  const [warning, setwarning] = useState(false);
+  const [disabled, setdisabled] = useState(true);
 
   const handleSearch = async (data) => {
     let query = data.target.value;
@@ -13,8 +16,44 @@ const Fullsearch = () => {
       const res = await Service.searchData(query);
       setloading(false);
       settableData(res);
+      setdisabled(false);
+    } else {
+      setwarning(true);
+      setdisabled(true);
+      setTimeout(() => {
+        setwarning(false);
+      }, 3000);
     }
   };
+
+  const handleSelect = (data) => {
+    if (tableData !== null) {
+      var obj = [...tableData];
+      switch (data.target.value) {
+        case "1":
+          settableData(obj.sort((a, b) => a.name.localeCompare(b.name)));
+          break;
+        case "2":
+          settableData(obj.sort((a, b) => b.name.localeCompare(a.name)));
+          break;
+        case "3":
+          settableData(obj.sort((a, b) => a.createdAt.split('/').reverse().join().localeCompare(b.createdAt.split('/').reverse().join())));
+          break;
+        case "4":
+          settableData(obj.sort((a, b) => b.createdAt.split('/').reverse().join().localeCompare(a.createdAt.split('/').reverse().join())));
+          break;
+        default:
+          break;
+      }
+    }
+  };
+
+  const options = [
+    { label: "Name ascending", id: "1" },
+    { label: "Name descending", id: "2" },
+    { label: "Year ascending", id: "3" },
+    { label: "Year descending", id: "4" },
+  ];
 
   return (
     <div style={{ position: "relative", height: "100vh", padding: "30px" }}>
@@ -29,10 +68,24 @@ const Fullsearch = () => {
         <button className="fullSearchButton">
           <label className="search">Search</label>
         </button>
+        <p
+          hidden={!warning}
+          style={{ color: "red", textAlign: "center", marginRight: "10vw" }}
+        >
+          Aranacak içerik en az 3 harf veya rakam olmalıdır.
+        </p>
+      </div>
+      <div className="sorter">
+      <img style={{position:"relative",top:"5px"}} src={sorter}/>
+        <select placeholder={"Order By"} disabled={disabled} onChange={handleSelect}>
+          {options.map((opt) => (
+            <option value={opt.id}>{opt.label}</option>
+          ))}
+        </select>
       </div>
       <div className="table">
         <div hidden={!loading} className="loading"></div>
-        {tableData.slice(0, 5).map((obj) => (
+        {tableData.map((obj) => (
           <div key={obj.id}>
             <p>{obj.name}</p>
             <span>{obj.title}</span>
